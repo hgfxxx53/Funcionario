d3524903e6ec6d4c3a2b90d68aff1a514803eacd
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-11-29 07:34:18 +0000

    Update App.java

--- src/main/java/com/alfinhe/App.java
+++ src/main/java/com/alfinhe/App.java
@@ -1,4 +1,18 @@
 package com.alfinhe;
 
 public class App {
+   public static void main( String[] args ) {
+        Empresa empresa = new Empresa ( );
+
+        Funcionario funcionario1 = new Funcionario ( "João", 2000.0 );
+        Funcionario funcionario2 = new Funcionario ( "Maria", 2500.0 );
+        Funcionario funcionario3 = new Funcionario ( "Pedro", 1800.0 );
+        empresa.adicionarFuncionario (funcionario1);
+        empresa.adicionarFuncionario (funcionario2);
+        empresa.adicionarFuncionario (funcionario3);
+
+        double valorMensal = empresa.calcularValorMensal ( );
+        System.out.println("Valor mensal total dos funcionários: " + valorMensal);
+    }
 }
+
