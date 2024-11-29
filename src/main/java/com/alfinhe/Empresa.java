46834669667f4dd07c51f92ab84a33e15adf1d7d
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-11-29 07:35:17 +0000

    Update Empresa.java

--- src/main/java/com/alfinhe/Empresa.java
+++ src/main/java/com/alfinhe/Empresa.java
@@ -1,4 +1,21 @@
 package com.alfinhe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empresa {
+    private List <Funcionario> funcionarios;
+    public Empresa() {
+        this.funcionarios = new ArrayList <> ();
+    }
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+    public double calcularValorMensal() {
+        double valorTotal = 0;
+        for (Funcionario funcionario : funcionarios) {
+            valorTotal += funcionario.getSalarioMensal();
+        }
+        return valorTotal;
+    }
 }
