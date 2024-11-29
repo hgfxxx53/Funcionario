04351f551a5846245fbb2575f9b39d61db866649
Ualfinhe Barbosa <147889889+hgfxxx53@users.noreply.github.com>
2024-11-29 07:36:24 +0000

    Update Funcionario.java

--- src/main/java/com/alfinhe/Funcionario.java
+++ src/main/java/com/alfinhe/Funcionario.java
@@ -1,4 +1,15 @@
 package com.alfinhe;
 
 public class Funcionario {
+      private String nome;
+    private double salarioMensal;
+
+    public Funcionario(String nome, double salarioMensal) {
+        this.nome = nome;
+        this.salarioMensal = salarioMensal;
+
+}
+    public double getSalarioMensal() {
+        return salarioMensal;
+    }
 }
